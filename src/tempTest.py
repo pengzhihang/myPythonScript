c56285bd195e5d245d1510c142bdb38d98ef3fc7
Administrator <Administrator@PC-20190818ERKX>
2019-10-31 15:36:55 +0800

    test1

--- src/tempTest.py
+++ src/tempTest.py
@@ -6,4 +6,18 @@ re=requests.get('http://www.baidu.com')
 
 print re.text
 
+import unittest
 
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def testMet1(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
